60c87a30f4fe165590370cb2acf3a2b76e91af99
Aryan Raval <aryanraval666@gmail.com>
2025-03-28 17:57:14 +0530

    commit

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from 'react-router-dom'
-import Header from '../components/header/header'
+import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import ScrollToTop from '../components/ScrollToTop'
 
